695ea5e6257f0734ab3a011316a370a40cef094e
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-04-17 17:15:06 -0300

    feat: add empty entity scaffolding for remaining domain classes

--- Ticketing-ProyectoSofware/TicketingSystem.Domain/Enums/SeatStatus.cs
+++ Ticketing-ProyectoSofware/TicketingSystem.Domain/Enums/SeatStatus.cs
@@ -1,11 +1,5 @@
 namespace TicketingSystem.Domain.Enums;
 
-/// <summary>
-/// Define los estados posibles de una butaca.
-/// EF Core persiste estos valores como string en la BD ("Available", "Reserved", "Sold")
-/// para respetar el diagrama del TP, manteniendo type safety en el código.
-/// </summary>
-/// 
 public enum SeatStatus
 {
     Available = 0,
