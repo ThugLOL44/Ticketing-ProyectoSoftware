3970dee81d7e4d42450e5ae86730367437f03d91
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-04-17 17:32:03 -0300

    feat: add AppDbContext with entity event and sector registration

---
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Sector> Sectors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
