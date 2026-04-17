875181621d66484ed3004d757cd6d1b6e5d96e3e
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-04-17 17:52:31 -0300

    feat: add fluent api mappings for event and sector tables

---
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/Mappings/SectorMapping.cs
@@ -0,0 +1,28 @@
+﻿using TicketingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketingSystem.Infrastructure.Persistence.Mappings
+{
+    public class SectorMapping : IEntityTypeConfiguration<Sector>
+    {
+        public void Configure(EntityTypeBuilder<Sector> builder)
+        {
+            builder.ToTable("Sectors");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(s => s.Price)
+                .IsRequired()
+                .HasColumnType("decimal(10,2)");
+
+            builder.Property(s => s.Capacity)
+                .IsRequired();
+
+        }
+    }
+}
