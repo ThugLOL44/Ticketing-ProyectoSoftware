45665e23ffd912f9cdfed99773b867de4962562b
luly_g24 <gonzalezludmila36@gmail>
2026-04-18 01:10:06 -0300

    feat: add seat, reservation, user and auditlog mappings

---
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/Mappings/ReservationMapping.cs
@@ -0,0 +1,38 @@
+﻿using TicketingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketingSystem.Infrastructure.Persistence.Mappings
+{
+    public class ReservationMapping : IEntityTypeConfiguration<Reservation>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            builder.ToTable("Reservations");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Status)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasDefaultValue("Pending");
+
+            builder.Property(r => r.ReservedAt)
+                .IsRequired();
+
+            builder.Property(r => r.ExpiresAt)
+                .IsRequired();
+
+
+            builder.HasOne(r => r.Seat)
+                .WithOne(s => s.ActiveReservation)
+                .HasForeignKey<Reservation>(r => r.SeatId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(r => r.User)
+                .WithMany(u => u.Reservations)
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
