153a49df525b804c6e1dfee19758937ff1b1ce8f
luly_g24 <gonzalezludmila36@gmail>
2026-04-18 00:41:06 -0300

    feat: add user, seat, reservation and auditlog entities

--- Ticketing-ProyectoSofware/TicketingSystem.Domain/Entities/Reservation.cs
+++ Ticketing-ProyectoSofware/TicketingSystem.Domain/Entities/Reservation.cs
@@ -1,6 +1,15 @@
 namespace TicketingSystem.Domain.Entities;
 
+
 public class Reservation
 {
+    public Guid Id { get; set; }
+    public Guid SeatId { get; set; }
+    public Guid UserId { get; set; }
+    public string Status { get; set; } = "Pending";
+    public DateTimeOffset ReservedAt { get; set; }
+    public DateTimeOffset ExpiresAt { get; set; }
 
+    public Seat Seat { get; set; } = null!;
+    public User User { get; set; } = null!;
 }
