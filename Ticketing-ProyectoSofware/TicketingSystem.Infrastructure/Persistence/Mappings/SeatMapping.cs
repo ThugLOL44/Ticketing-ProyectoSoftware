d97ee704d65639d8ae94050af117c359877dd268
luly_g24 <gonzalezludmila36@gmail>
2026-04-18 01:10:06 -0300

    feat: add seat, reservation, user and auditlog mappings

---
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/Mappings/SeatMapping.cs
@@ -0,0 +1,40 @@
+﻿using TicketingSystem.Domain.Entities;
+using TicketingSystem.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketingSystem.Infrastructure.Persistence.Mappings
+{   /// </summary>
+    /// Version actúa como token de concurrencia para Optimistic Locking (Entrega 2).
+    /// </summary>
+    public class SeatMapping : IEntityTypeConfiguration<Seat>
+    {
+        public void Configure(EntityTypeBuilder<Seat> builder)
+        {
+            builder.ToTable("Seats");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.RowIdentifier)
+                .IsRequired()
+                .HasMaxLength(5);
+
+            builder.Property(s => s.SeatNumber)
+                .IsRequired();
+
+            builder.Property(s => s.Status)
+                .IsRequired()
+                .HasConversion<string>()
+                .HasDefaultValue(SeatStatus.Available);
+
+            // Token de concurrencia para Optimistic Locking
+            builder.Property(s => s.Version)
+                .IsConcurrencyToken();
+
+            builder.HasOne(s => s.Sector)
+                .WithMany(sec => sec.Seats)
+                .HasForeignKey(s => s.SectorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/Mappings/UserMapping.cs
@@ -0,0 +1,30 @@
+﻿using TicketingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketingSystem.Infrastructure.Persistence.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder.Property(u => u.PasswordHash)
+                .IsRequired();
+        }
+    }
+}
