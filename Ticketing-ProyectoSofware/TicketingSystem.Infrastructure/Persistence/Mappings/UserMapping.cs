736388ae0111a9a1d38a88fc30bc2fd09a957257
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-04-18 17:05:56 -0300

    feat: configure program.cs and database connection

--- Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/Mappings/UserMapping.cs
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Persistence/Mappings/UserMapping.cs
@@ -1,30 +1,30 @@
-﻿using TicketingSystem.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿    using TicketingSystem.Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace TicketingSystem.Infrastructure.Persistence.Mappings
-{
-    public class UserMapping : IEntityTypeConfiguration<User>
+    namespace TicketingSystem.Infrastructure.Persistence.Mappings
     {
-        public void Configure(EntityTypeBuilder<User> builder)
+        public class UserMapping : IEntityTypeConfiguration<User>
         {
-            builder.ToTable("Users");
+            public void Configure(EntityTypeBuilder<User> builder)
+            {
+                builder.ToTable("Users");
 
-            builder.HasKey(u => u.Id);
+                builder.HasKey(u => u.Id);
 
-            builder.Property(u => u.Name)
-                .IsRequired()
-                .HasMaxLength(100);
+                builder.Property(u => u.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
 
-            builder.Property(u => u.Email)
-                .IsRequired()
-                .HasMaxLength(200);
+                builder.Property(u => u.Email)
+                    .IsRequired()
+                    .HasMaxLength(200);
 
-            builder.HasIndex(u => u.Email)
-                .IsUnique();
+                builder.HasIndex(u => u.Email)
+                    .IsUnique();
 
-            builder.Property(u => u.PasswordHash)
-                .IsRequired();
+                builder.Property(u => u.PasswordHash)
+                    .IsRequired();
+            }
         }
     }
-}
