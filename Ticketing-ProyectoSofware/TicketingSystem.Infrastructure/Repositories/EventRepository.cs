004382ecef04156bca8b9ad612afb2869d14400d
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-04-19 17:36:46 -0300

    feat: add event repository, service and interfaces

---
+++ Ticketing-ProyectoSofware/TicketingSystem.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Application.DTOs;
+using TicketingSystem.Application.Interfaces;
+using TicketingSystem.Infrastructure.Persistence;
+
+namespace TicketingSystem.Infrastructure.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EventRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<(IEnumerable<EventDto> Events, int TotalCount)> GetPagedEventsAsync(int page, int pageSize)
+        {
+            var query = _context.Events
+                .Where(e => e.Status == "Active")
+                .OrderBy(e => e.EventDate);
+
+            var totalCount = await query.CountAsync();
+
+            var events = await query
+                .Skip((page - 1) * pageSize)    
+                .Take(pageSize)
+                .Select(e => new EventDto
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Venue = e.Venue,
+                    EventDate = e.EventDate,
+                    status = e.Status
+                })
+                .ToListAsync();
+
+            return (events, totalCount);
+        }
+
+    }
+}
