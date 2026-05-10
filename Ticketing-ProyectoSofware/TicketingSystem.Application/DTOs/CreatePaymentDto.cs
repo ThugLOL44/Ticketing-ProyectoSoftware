4c32a3483579589f5f770aff957e9699ab5e2c42
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-05-10 13:05:19 -0300

    feat(payments): Add IPaymentService, NotFoundException, CreatePaymentDTO and PaymentController

---
+++ Ticketing-ProyectoSofware/TicketingSystem.Application/DTOs/CreatePaymentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Application.DTOs
+{
+    public class CreatePaymentDto
+    {
+        public Guid ReservationId { get; set; }
+    }
+}
---
+++ Ticketing-ProyectoSofware/TicketingSystem.Application/Interfaces/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Application.Interfaces
+{
+    internal interface IPaymentService
+    {
+    }
+}
