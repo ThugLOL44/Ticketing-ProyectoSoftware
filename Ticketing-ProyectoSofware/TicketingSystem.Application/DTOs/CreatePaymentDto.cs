ddf2066d877c7bbfe624cd9b7c63a49f9e37c9e2
ThugLOL44 <capogeniofigura1234@gmail.com>
2026-05-11 13:11:37 -0300

    fix(payments): update cartPanel classes, move cart to right side and clean DTO usings

--- Ticketing-ProyectoSofware/TicketingSystem.Application/DTOs/CreatePaymentDto.cs
+++ Ticketing-ProyectoSofware/TicketingSystem.Application/DTOs/CreatePaymentDto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TicketingSystem.Application.DTOs
+﻿namespace TicketingSystem.Application.DTOs
 {
     public class CreatePaymentDto
     {
